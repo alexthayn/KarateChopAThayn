6dc929ca5cbfca22f5af4b56e755e35acff66d42
alexthayn <alexthayn>
2018-08-27 13:20:55 -0600

    push

---
+++ BinarySearchTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinarySearchTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinarySearchTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("48816301-4402-4a98-b811-a95345046c69")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BinarySearch_AThayn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch_AThayn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private int Chop(int target, List<int> sortedArray)
+        {
+            List<int> tempArray = sortedArray;
+
+            for(int i = 0; i < Math.Ceiling(Math.Log(sortedArray.Count, 2)); i++)
+            {
+                int middleIndex = (tempArray.Count / 2);
+                if (tempArray[middleIndex] == target)
+                    return middleIndex;
+                if (tempArray[middleIndex] < target)
+                    tempArray = tempArray.GetRange(middleIndex + 1, tempArray.Count - 1);
+                else
+                    tempArray = tempArray.GetRange(0, middleIndex - 1);
+            }
+            return -1;
+        }
+
+        private int Chop1(int target, List<int> sortedArray)
+        {
+            return -1;
+        }
+    }
+}
